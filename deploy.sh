6c422ba2b426de04a39bb38081f90509208c8b53
garrijuan <garrijuan@gmail.com>
2023-08-31 12:46:15 +0200

    New version

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 helm package ./EoloPlanner -d ./EoloPlanner/charts
 
 # Create or update the Helm repository index
-helm repo index ./EoloPlanner/charts --url helm repo add httpd-web-server https://raw.githubusercontent.com/garrijuan/kubernetes-Security-Helm/main/EoloPlanner/charts/
+helm repo index ./EoloPlanner/charts #--url helm repo add httpd-web-server https://raw.githubusercontent.com/garrijuan/kubernetes-Security-Helm/main/EoloPlanner/charts/
 
 # Add the Helm repository
 helm repo add httpd-web-server helm repo add httpd-web-server https://raw.githubusercontent.com/garrijuan/kubernetes-Security-Helm/main/EoloPlanner/charts/
